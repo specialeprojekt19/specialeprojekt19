abd9de9cee3d4e6a71af0920af2cc7a4b7234634
specialeprojekt19 <49191138+specialeprojekt19@users.noreply.github.com>
2019-04-09 11:44:13 +0200

    Update startnet.sh

--- startnet.sh
+++ startnet.sh
@@ -1,2 +1,6 @@
+#In terminal:
+#sudo -i
+#nano startnet.sh
+
 #!/bin/bash
 curl --header "Referer: http://192.168.32.1/index.html" http://192.168.32.1/goform/goform_set_cmd_process?goformId=CONNECT_NETWORK
